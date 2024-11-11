98919f321db6dfffcd1f776758ceb46ee3feae12
1334713380@qq.com <wodetuan2012>
2024-11-11 21:47:36 +0800

    feat: 优化menu

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,7 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 
-use tauri_plugin_sql::{Builder, Migration, MigrationKind};
+use tauri_plugin_sql::{ Migration, MigrationKind};
+use tauri::{AppHandle, Manager};
 
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -16,15 +17,29 @@ pub fn run() {
             description: "create_initial_tables",
             sql: "CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT);",
             kind: MigrationKind::Up,
-        }
+        },
     ];
-    
+
     tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_sql::Builder::default()
-            .add_migrations("sqlite:mydatabase.db", migrations)
-            .build())
+        .plugin(
+            tauri_plugin_sql::Builder::default()
+                .add_migrations("sqlite:mydatabase.db", migrations)
+                .build(),
+        )
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn show_window(app: &AppHandle) {
+    let windows = app.webview_windows();
+
+    windows
+        .values()
+        .next()
+        .expect("Sorry, no window found")
+        .set_focus()
+        .expect("Can't Bring Window to Focus");
 }
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,10 +1,18 @@
-import {Button, Layout, Menu, theme} from "antd";
+import {Layout, Menu, theme} from "antd";
 import {useState} from "react";
 import {Link, Route, Routes} from "react-router-dom";
-import {MenuFoldOutlined, MenuUnfoldOutlined} from "@ant-design/icons";
+import {
+    BookOutlined,
+    FormOutlined,
+    HighlightOutlined,
+    SettingOutlined
+} from "@ant-design/icons";
 import ChatAIOptionPage from "../pages/Option/ChatAI";
 import StorageOptionPage from "../pages/Option/Storage";
 import Main from "../pages/Main";
+import BooksPage from "../pages/Books";
+import NotesPage from "../pages/Notes";
+import HighlightPage from "../pages/Highlight";
 
 
 const LayoutWrapper = () => {
@@ -13,34 +21,56 @@ const LayoutWrapper = () => {
     } = theme.useToken();
     const [collapsed, setCollapsed] = useState(false);
 
+
     return (
         <Layout style={{minHeight: '100vh'}}>
-            <Layout.Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-                <Menu>
-                    <Menu.Item>
-                        <Link to="options/chat-ai">Chat AI</Link>
-                    </Menu.Item>
-                    <Menu.Item>
-                        <Link to="options/storage">Storage</Link>
-                    </Menu.Item>
-                </Menu>
+            <Layout.Sider collapsible style={{background: colorBgContainer}} collapsed={collapsed}
+                          theme="light"
+                          onCollapse={(value) => setCollapsed(value)}>
+                <Menu mode="inline"
+                      theme="light"
+                      items={[
+                          {
+                              key: 'books',
+                              icon: <BookOutlined/>,
+                              label: <Link to={"books"}>全部图书</Link>,
+                          },
+                          {
+                              key: 'notes',
+                              icon: <FormOutlined/>,
+                              label: <Link to={"books"}>我的笔记</Link>,
+                          },
+                          {
+                              key: 'highlight',
+                              icon: <HighlightOutlined/>,
+                              label: <Link to={"highlight"}>我的划线</Link>,
+                          },
+                          {
+                              key: 'options',
+                              icon: <SettingOutlined/>,
+                              label: '系统设置',
+                              children: [
+                                  {
+                                      key: 'chat-ai',
+                                      label: <Link to="options/chat-ai">AI 设置</Link>,
+                                  },
+                                  {
+                                      key: 'storage',
+                                      label: <Link to="options/storage">存储设置</Link>,
+                                  },
+                              ],
+                          }
+                      ]}/>
             </Layout.Sider>
             <Layout>
                 <Layout.Header style={{padding: 0, background: colorBgContainer}}>
-                    <Button
-                        type="text"
-                        icon={collapsed ? <MenuUnfoldOutlined/> : <MenuFoldOutlined/>}
-                        onClick={() => setCollapsed(!collapsed)}
-                        style={{
-                            fontSize: '16px',
-                            width: 64,
-                            height: 64,
-                        }}
-                    />
                 </Layout.Header>
                 <Layout.Content style={{margin: '0 16px'}}>
                     <Routes>
-                        <Route path="/" element={<Main />}/>
+                        <Route path="/" element={<Main/>}/>
+                        <Route path="books" element={<BooksPage/>}/>
+                        <Route path="notes" element={<NotesPage/>}/>
+                        <Route path="highlight" element={<HighlightPage/>}/>
                         <Route path="options/chat-ai" element={<ChatAIOptionPage/>}/>
                         <Route path="options/storage" element={<StorageOptionPage/>}/>
                     </Routes>
