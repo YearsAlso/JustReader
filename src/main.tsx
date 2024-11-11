7224ccf5977294094ee55d4a362b9f7a3cd629df
1334713380@qq.com <wodetuan2012>
2024-11-11 21:54:10 +0800

    feat: 加入configProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import {ConfigProvider} from "antd";
+import zhCN from 'antd/locale/zh_CN';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ConfigProvider locale={zhCN} componentSize={"large"}>
+            <App/>
+        </ConfigProvider>
+    </React.StrictMode>,
 );
